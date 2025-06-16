bfb94c5e441559de8619f8f031580c11f7409d5a
ganyariya <ganariya2525@gmail.com>
2025-06-16 09:04:26 +0900

    commit

--- frontend/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all types for easy importing
-export * from './session';
-
-// Common utility types
-export type StringLiteral<T> = T extends string ? string extends T ? never : T : never;
